899dc75bf38054fb1a056601b49156bc6ee5778c
Santiago Arce <santiagoarcez65@gmail.com>
2025-03-03 16:18:22 -0500

    feat: deploy gith pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/genuine-front',
+  assetPrefix: '/genuine-front/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
